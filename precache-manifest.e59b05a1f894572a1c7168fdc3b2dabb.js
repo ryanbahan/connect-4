6ec32b0a26c164193cafa59b5ac1defe3ced833f
Ryan Bahan <ryan@ryanbahan.com>
2020-07-29 12:14:16 -0600

    Updates

---
+++ precache-manifest.e59b05a1f894572a1c7168fdc3b2dabb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5e13a0b8dfa221372bb1dee85ea103e",
+    "url": "/connect-4/index.html"
+  },
+  {
+    "revision": "7d8bcd1c6dbe9d9dc063",
+    "url": "/connect-4/static/css/main.0cbda255.chunk.css"
+  },
+  {
+    "revision": "491e0e07436e61a7f895",
+    "url": "/connect-4/static/js/2.a7187770.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/connect-4/static/js/2.a7187770.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d8bcd1c6dbe9d9dc063",
+    "url": "/connect-4/static/js/main.514e3c66.chunk.js"
+  },
+  {
+    "revision": "cf9991ce6220bff4236d",
+    "url": "/connect-4/static/js/runtime-main.61c0b5f4.js"
+  }
+]);
